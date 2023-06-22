3d7b4affea46908fd1a8ab7a4e3afc08c24703c6
Quanxing Liu <liuqx0717@gmail.com>
2023-06-21 18:33:33 -0700

    Accept additional build parameters in test-all.sh

--- test-all.sh
+++ test-all.sh
@@ -4,6 +4,7 @@ set -ex
 
 test-one-build () {
     CFG="$1"
+    shift
     BUILDDIR="build-$CFG"
 
     rm -rf -- "$BUILDDIR"
@@ -11,20 +12,20 @@ test-one-build () {
     pushd -- "$BUILDDIR"
 
     # Run unit tests.
-    cmake -DCMAKE_BUILD_TYPE="$CFG" ..
+    cmake "$@" -DCMAKE_BUILD_TYPE="$CFG" ..
     make -j8
     make test
 
     # Run unit tests with valgrind.
-    cmake -DCMAKE_BUILD_TYPE="$CFG" -DENABLE_ASAN=0 ..
+    cmake "$@" -DCMAKE_BUILD_TYPE="$CFG" -DENABLE_ASAN=0 ..
     make -j8 unit-test
     ctest -T memcheck
 
     popd
 }
 
-test-one-build Debug
-test-one-build Release
+test-one-build Debug "$@"
+test-one-build Release "$@"
 
 echo "All tests passed."
 
